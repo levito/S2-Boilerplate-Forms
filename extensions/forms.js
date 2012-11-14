b3ed5c8d0ad360746214b5703ad3e6439f3084d5
Veit Lehmann <veit.lehmann@googlemail.com>
2012-11-14 19:48:01 +0100

    mobile optimization, html5 forms, client side form validation via jquery tools validator

--- extensions/forms.js
+++ extensions/forms.js
@@ -1,4 +1,13 @@
 if ($.browser.msie && $.browser.version < 9) {
+	// style new html5 input types
+	// $("input").each(function() {
+	// 	var $input = $(this);
+	// 	if (this.type == "text" && $input.attr("type") != "text") {
+	// 		// TODO: replace form fields by copies with type=text (IE8 doesn't allow styling of HTML5 form types)
+	// 	}
+	// });
+
+	// CSS :checked simulation
 	$(document).on("click", ":radio, :checkbox", function() {
 		var $input = $(this);
 		if ($input.is(":checked")) {
@@ -11,3 +20,25 @@ if ($.browser.msie && $.browser.version < 9) {
 		}
 	});
 }
+
+
+/**
+ * jQuery Tools Validator
+ */
+$.tools.validator.localize("de", {
+	"[required]": "Bitte füllen Sie diese Feld aus."
+});
+
+$("form").validator({
+	lang: "de",
+	messageClass: "invalidmsg",
+	position: "top right"
+});
+
+
+/**
+ * jQuery Validate
+ */
+// $("form").validate({
+// 	errorElement: "span"
+// });
