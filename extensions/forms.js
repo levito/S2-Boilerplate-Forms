b82b95d38a7547b688e5a1d7fd10e3c794ff1879
Veit Lehmann <veit.lehmann@googlemail.com>
2012-11-15 19:33:23 +0100

    jquery validate instead of jquery tools validator, IE8 and Opera fixes

--- extensions/forms.js
+++ extensions/forms.js
@@ -8,7 +8,7 @@ if ($.browser.msie && $.browser.version < 9) {
 	// });
 
 	// CSS :checked simulation
-	$(document).on("click", ":radio, :checkbox", function() {
+	var checkedPolyfill = function() {
 		var $input = $(this);
 		if ($input.is(":checked")) {
 			if ($input.is(":radio")) {
@@ -18,27 +18,91 @@ if ($.browser.msie && $.browser.version < 9) {
 		} else if ($input.is(":checkbox")) {
 			$input.removeClass("checked");
 		}
+	}
+	$(document).on("click", ":radio, :checkbox", checkedPolyfill);
+	$(":radio, :checkbox").each(checkedPolyfill);
+}
+
+
+/**
+ * Placeholder polyfill
+ */
+if (!("placeholder" in document.createElement("input"))) {
+	$("input[placeholder], textarea[placeholder]").each(function() {
+		var $input = $(this);
+		$input.data("placeholder", $input.attr("placeholder"));
+		$input.removeAttr("placeholder");
+
+		var $clone = $input.clone();
+		if ($.browser.msie && $clone.attr("type") == "password") {
+			$clone = $("<input type='text'/>");
+			$clone.attr("class", $input.attr("class"));
+			$clone.attr("size", $input.attr("size"));
+		}
+		$clone.data("placeholder", $input.data("placeholder"));
+		$clone.attr("readonly", "readonly");
+		$clone.val($input.data("placeholder"));
+		$clone.removeAttr("name");
+		$input.removeAttr("id");
+		$clone.addClass("ignore placeholder");
+		if (!!$clone.attr("type") && $clone.attr("type").toLowerCase() == "password") {
+			$clone.attr("type", "text");
+		}
+		$clone.css("display", $input.css("display"));
+		$input.data("placeholder-input", $clone);
+		$clone.data("orig-input", $input);
+		$clone.hide();
+		$input.before($clone);
+
+		if (this.value == "") {
+			$input.hide();
+			$clone.show();
+		} else {
+			$clone.hide();
+			$input.show();
+		}
+
+		$clone.focus(function() {
+			var $placeholder = $(this);
+			var $input = $placeholder.data("orig-input");
+			var placeholderValue = $placeholder.val();
+			$placeholder.hide().val("").val(placeholderValue); // opera text selection fix
+			$input.show().focus();
+		});
+		$input.blur(function() {
+			if (this.value == "") {
+				var $placeholder = $input.data("placeholder-input");
+				$input.hide();
+				$placeholder.show();
+			}
+		});
 	});
 }
 
 
 /**
- * jQuery Tools Validator
- */
-$.tools.validator.localize("de", {
-	"[required]": "Bitte füllen Sie diese Feld aus."
-});
-
-$("form").validator({
-	lang: "de",
-	messageClass: "invalidmsg",
-	position: "top right"
-});
-
-
-/**
  * jQuery Validate
  */
-// $("form").validate({
-// 	errorElement: "span"
-// });
+$.validator.addMethod("pattern", function(value, element, param) {
+	if (this.optional(element)) {
+		return true;
+	}
+	if (typeof param === 'string') {
+		param = new RegExp('^(?:' + param + ')$');
+	}
+	return param.test(value);
+}, "Ungültiges Format.");
+
+$("form").validate({
+	rules: {
+		//password: "required",
+		passwordagain: {
+			equalTo: "#password"
+		}
+	},
+	messages: {
+		zip: {
+			pattern: "Bitte geben Sie eine gültige Postleitzahl an."
+		}
+	}
+});
